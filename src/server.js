8897fce55007980cdffbdf92295ff0b4aa33bee5
Eduardo do Prado Cardoso <dpradoeduardo@gmail.com>
2022-04-03 19:33:20 -0300

    adjust heroku

--- src/server.js
+++ src/server.js
@@ -9,4 +9,4 @@ app.use(express.json());
 app.use(cors());
 app.use(productsRoutes)
 app.use(userRoutes)
-app.listen(PORT, () => console.log('listening on port :'+PORT));
+app.listen(80, () => console.log('listening on port :'+PORT));
